b69af09560cb323f952bfa5ee585cbeaf5d10095
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-26 20:45:23 +0200

    initial commit

--- 01.Working with Abstraction - Lab/P01.RhombusOfStars/Startup.cs
+++ 01.Working with Abstraction - Lab/P01.RhombusOfStars/Startup.cs
@@ -5,7 +5,31 @@
     {
         public static void Main()
         {
+            int size = int.Parse(Console.ReadLine());
             
+            for (int starCount = 0; starCount < size; starCount++)
+            {
+                PrintRow(size, starCount);
+            }
+
+            for (int starCount = size - 1; starCount >= 0; starCount--)
+            {
+                PrintRow(size, starCount);
+            }
+        }
+
+        private static void PrintRow(int figureSize, int starCount)
+        {
+            for (int i = 0; i < figureSize - starCount; i++)
+            {
+               Console.Write(" ");                
+            }
+
+            for (int col = 0; col < starCount; col++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine(" ");
         }
     }
 }
