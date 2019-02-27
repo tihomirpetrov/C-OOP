87a8edd1bd4edecc54a682f8e2bd09cd94801ba2
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-27 21:32:43 +0200

    initial commit

---
+++ 01.Working with Abstraction - Exercises/P01.RawData/Engine.cs
@@ -0,0 +1,15 @@
+﻿namespace P01.RawData
+{
+    public class Engine
+    {
+        private int speed;
+
+        public Engine(int speed, int power)
+        {
+            this.speed = speed;
+            this.Power = power;
+        }
+
+        public int Power { get; set; }
+    }
+}
