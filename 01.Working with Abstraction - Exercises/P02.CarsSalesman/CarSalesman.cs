8307e949e66952f87a7f35876246f934a143a8d4
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-27 23:10:51 +0200

    initial commit

---
+++ 01.Working with Abstraction - Exercises/P02.CarsSalesman/CarSalesman.cs
@@ -0,0 +1,72 @@
+﻿namespace P02.CarsSalesman
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CarSalesman
+    {
+        private List<Car> cars;
+        private List<Engine> engines;
+
+        public CarSalesman()
+        {
+            this.cars = new List<Car>();
+            this.engines = new List<Engine>();
+        }
+
+        public void AddEngine(string[] parameters)
+        {
+            string model = parameters[0];
+            int power = int.Parse(parameters[1]);
+
+            int displacement = -1;
+
+            if (parameters.Length == 3 && int.TryParse(parameters[2], out displacement))
+            {
+                engines.Add(new Engine(model, power, displacement));
+            }
+            else if (parameters.Length == 3)
+            {
+                string efficiency = parameters[2];
+                engines.Add(new Engine(model, power, efficiency));
+            }
+            else if (parameters.Length == 4)
+            {
+                string efficiency = parameters[3];
+                engines.Add(new Engine(model, power, int.Parse(parameters[2]), efficiency));
+            }
+            else
+            {
+                engines.Add(new Engine(model, power));
+            }
+        }
+
+        public void AddCar(string[] parameters)
+        {
+            string model = parameters[0];
+            string engineModel = parameters[1];
+            Engine engine = engines.FirstOrDefault(x => x.Model == engineModel);
+
+            int weight = -1;
+
+            if (parameters.Length == 3 && int.TryParse(parameters[2], out weight))
+            {
+                cars.Add(new Car(model, engine, weight));
+            }
+            else if (parameters.Length == 3)
+            {
+                string color = parameters[2];
+                cars.Add(new Car(model, engine, color));
+            }
+            else if (parameters.Length == 4)
+            {
+                string color = parameters[3];
+                cars.Add(new Car(model, engine, int.Parse(parameters[2]), color));
+            }
+            else
+            {
+                cars.Add(new Car(model, engine));
+            }
+        }
+    }
+}
