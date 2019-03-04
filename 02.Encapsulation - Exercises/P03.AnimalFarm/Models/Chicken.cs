8eecfc9d78c5f11e7d1a5535fc5a175f1f9484dc
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-03-04 14:51:11 +0200

    initial commit

--- 02.Encapsulation - Exercises/P03.AnimalFarm/Models/Chicken.cs
+++ 02.Encapsulation - Exercises/P03.AnimalFarm/Models/Chicken.cs
@@ -57,31 +57,31 @@
 
         public double GetProductPerDay()
         {
-           return this.CalculateProductPerDay();
+            return this.CalculateProductPerDay();
         }
 
-    public double CalculateProductPerDay()
-    {
-        switch (this.Age)
+        public double CalculateProductPerDay()
         {
-            case 0:
-            case 1:
-            case 2:
-            case 3:
-                return 1.5;
-            case 4:
-            case 5:
-            case 6:
-            case 7:
-                return 2;
-            case 8:
-            case 9:
-            case 10:
-            case 11:
-                return 1;
-            default:
-                return 0.75;
+            switch (this.Age)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                    return 1.5;
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                    return 2;
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                    return 1;
+                default:
+                    return 0.75;
+            }
         }
     }
-}
 }
