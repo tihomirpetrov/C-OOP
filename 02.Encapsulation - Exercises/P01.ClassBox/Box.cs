2ccc1938e07fd803525b7cab7b82e46b8836be48
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-03-04 12:46:55 +0200

    initial commit

---
+++ 02.Encapsulation - Exercises/P01.ClassBox/Box.cs
@@ -0,0 +1,66 @@
+﻿namespace P01.ClassBox
+{
+    using System.Text;
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get
+            {
+                return this.length;
+            }
+            set
+            {
+                this.length = value;
+            }
+        }
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            set
+            {
+                this.width = value;
+            }
+        }
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            set
+            {
+                this.height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            return (2 * this.length * this.width) + (2 * this.width * this.height) + (2 * this.length * this.height);
+        }
+
+        public double LateralSurfaceArea()
+        {
+            return (2 * this.length * this.height) + (2 * this.width * this.height);
+        }
+
+        public double Volume()
+        {
+            return this.length * this.width * this.height;
+        }
+    }
+}
--- 02.Encapsulation - Exercises/P01.ClassBox/Startup.cs
+++ 02.Encapsulation - Exercises/P01.ClassBox/Startup.cs
@@ -1,11 +1,19 @@
 ﻿namespace P01.ClassBox
 {
     using System;
-    public class Program
+    public class Startup
     {
         public static void Main()
         {
-            
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            Box box = new Box(length, width, height);
+
+            Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+            Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+            Console.WriteLine($"Volume - {box.Volume():f2}");
         }
     }
 }
