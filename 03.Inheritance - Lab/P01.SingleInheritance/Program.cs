a2b09f977fb0cc75ac4d7aa32e5c85d6c005f932
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-03-07 21:09:29 +0200

    initial commit

---
+++ 03.Inheritance - Lab/P01.SingleInheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01.SingleInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
