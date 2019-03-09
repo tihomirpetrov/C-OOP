aa32b8e645c76da4f8ae9837a9444486c67edc33
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-03-09 21:47:29 +0200

    initial commit

--- 03.Inheritance - Exercises/P03.Mankind/Student.cs
+++ 03.Inheritance - Exercises/P03.Mankind/Student.cs
@@ -1,6 +1,35 @@
-﻿namespace P03.Mankind
+﻿using System;
+using System.Linq;
+
+namespace P03.Mankind
 {
-    public class Student
+    public class Student : Human
     {
+        private string facultyNumber;
+
+        public Student(string firstName, string lastName, string facultyNumber) : base(firstName, lastName)
+        {
+            this.FacultyNumber = facultyNumber;
+        }
+        public string FacultyNumber
+        {
+            get
+            {
+               return this.facultyNumber;
+            }
+            set
+            {
+                if (value.Length < 5 || value.Length > 10)
+                {
+                    throw new ArgumentException("Invalid faculty number!");
+                }
+                if (value.All(x => char.IsLetterOrDigit(x)))
+                {
+                    throw new ArgumentException("Invalid faculty number!");
+                }
+
+                this.facultyNumber = value;
+            }
+        }
     }
 }
--- 03.Inheritance - Exercises/P03.Mankind/Worker.cs
+++ 03.Inheritance - Exercises/P03.Mankind/Worker.cs
@@ -1,6 +1,51 @@
 ﻿namespace P03.Mankind
 {
-    public class Worker
+    using System;
+    public class Worker : Human
     {
+        private double weekSalary;
+        private double workHoursPerDay;
+
+        public Worker(string firstName, string lastName, double weekSalary, double workHoursPerDay) : base(firstName, lastName)
+        {
+            this.WeekSalary = weekSalary;
+            this.WorkHoursPerDay = workHoursPerDay;
+        }
+        public double WeekSalary
+        {
+            get
+            {
+                return this.weekSalary;
+            }
+            set
+            {
+                if (value < 10)
+                {
+                    throw new ArgumentException("Expected value mismatch! Argument: weekSalary");
+                }
+                this.weekSalary = value;
+            }
+        }
+
+        public double WorkHoursPerDay
+        {
+            get
+            {
+                return this.workHoursPerDay;
+            }
+            set
+            {
+                if (value < 1 || value > 12)
+                {
+                    throw new ArgumentException("Expected value mismatch! Argument: workHoursPerDay");
+                }
+                this.workHoursPerDay = value;
+            }
+        }
+
+        public double CalculateSalaryPerHour()
+        {
+            return this.weekSalary / this.workHoursPerDay * 5;
+        }
     }
 }
