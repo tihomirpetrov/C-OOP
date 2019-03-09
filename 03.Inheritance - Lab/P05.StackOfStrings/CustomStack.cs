dfadd0d5cf72790385d473a53ec8ac453943c406
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-03-09 17:36:18 +0200

    initial commit

---
+++ 03.Inheritance - Lab/P05.StackOfStrings/CustomStack.cs
@@ -0,0 +1,20 @@
+﻿namespace CustomStack
+{
+    using System.Collections;
+
+    public class CustomStack : Stack
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public void AddRange(params string[] data)
+        {
+            foreach (var item in data)
+            {
+                this.Push(item);
+            }
+        }
+    }
+}
---
+++ 03.Inheritance - Lab/P05.StackOfStrings/Startup.cs
@@ -0,0 +1,12 @@
+﻿namespace CustomStack
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CustomStack stack = new CustomStack();
+
+            stack.AddRange("1", "2", "3");
+        }
+    }
+}
