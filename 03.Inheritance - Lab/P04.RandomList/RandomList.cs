29f21f79d6875b4f7dc5ff0c66391aa12f3b3dd3
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-03-09 17:05:00 +0200

    initial commit

---
+++ 03.Inheritance - Lab/P04.RandomList/RandomList.cs
@@ -0,0 +1,27 @@
+﻿namespace CustomRandomList
+{
+    using System;
+    using System.Collections.Generic;
+    public class RandomList : List<string>
+    {
+        private Random random;
+
+        public RandomList()
+        {
+            this.random = new Random();
+        }
+
+        public string RandomString()
+        {
+            string element = null;
+            if (this.Count > 0)
+            {
+                int index = this.random.Next(0, this.Count);
+                element = this[index];
+                this.RemoveAt(index);
+            }
+
+            return element;
+        }
+    }
+}
---
+++ 03.Inheritance - Lab/P04.RandomList/Startup.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomRandomList
+{
+    using System;
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var randomList = new RandomList();
+            randomList.Add("Test");
+            randomList.Add("Ivan");
+            randomList.Add("Pesho");
+
+            Console.WriteLine(randomList.RandomString());
+        }
+    }
+}
