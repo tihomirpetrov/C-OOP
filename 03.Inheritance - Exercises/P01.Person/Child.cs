49d4ac9208a089f26506af52a23bce6294177016
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-03-09 19:27:52 +0200

    initial commit

---
+++ 03.Inheritance - Exercises/P01.Person/Child.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace P01.Person
+{
+    public class Child : Person
+    {
+        private string name;
+        private int age;
+        public Child(string name, int age) : base(name, age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value > 15)
+                {
+                    throw new ArgumentException("Child's age must be less than 15!");
+                }
+            }
+        }
+    }
+}
--- 03.Inheritance - Exercises/P01.Person/Startup.cs
+++ 03.Inheritance - Exercises/P01.Person/Startup.cs
@@ -5,7 +5,18 @@
     {
         public static void Main()
         {
-            
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Child child = new Child(name, age);
+                Console.WriteLine(child);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
         }
     }
 }
