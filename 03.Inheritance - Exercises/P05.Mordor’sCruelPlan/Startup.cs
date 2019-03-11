ea6196bb1e1ad8b6b054cf8f44f3ca08da560030
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-03-11 18:04:52 +0200

    initial commit

--- 03.Inheritance - Exercises/P05.Mordor’sCruelPlan/Startup.cs
+++ 03.Inheritance - Exercises/P05.Mordor’sCruelPlan/Startup.cs
@@ -1,6 +1,7 @@
 ﻿namespace P05.Mordor_sCruelPlan
 {
     using P05.Mordor_sCruelPlan.Foods;
+    using P05.Mordor_sCruelPlan.Moods;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -40,13 +41,25 @@
                 }
             }
 
-            int happinessFood = foodsEaten.Where(c=>c != null).Select(x =>x == null ? -1: x.Happiness).Sum();
+            int happinessFood = foodsEaten.Select(x => x == null ? -1 : x.Happiness).Sum();
 
             Console.WriteLine(happinessFood);
 
             if (happinessFood < -5)
             {
-
+                Console.WriteLine(nameof(Angry));
+            }
+            else if (happinessFood >= -5 && happinessFood <= 0)
+            {
+                Console.WriteLine(nameof(Sad));
+            }
+            else if (happinessFood >= 1 && happinessFood <= 15)
+            {
+                Console.WriteLine(nameof(Happy));
+            }
+            else if (happinessFood > 15)
+            {
+                Console.WriteLine(nameof(JavaScript));
             }
         }
     }
