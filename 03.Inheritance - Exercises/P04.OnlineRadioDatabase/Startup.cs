d89ab13c097f00c7c8ac064a885028b2a12667bb
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-03-11 11:01:29 +0200

    initial commit

--- 03.Inheritance - Exercises/P04.OnlineRadioDatabase/Startup.cs
+++ 03.Inheritance - Exercises/P04.OnlineRadioDatabase/Startup.cs
@@ -16,23 +16,23 @@
         {
             Console.WriteLine($"Songs added: {playlist.Count}");
 
-            var totalSeconds = playlist.Select(s => s.SongSeconds).Sum();
-            var secondsToMinutes = totalSeconds / 60;
-            var seconds = totalSeconds % 60;
+            int totalSeconds = playlist.Select(s => s.SongSeconds).Sum();
+            int secondsToMinutes = totalSeconds / 60;
+            int seconds = totalSeconds % 60;
 
-            var totalMinutes = playlist.Select(s => s.SongMinutes).Sum() + secondsToMinutes;
-            var minutesToHours = totalMinutes / 60;
-            var minutes = totalMinutes % 60;
+            int totalMinutes = playlist.Select(s => s.SongMinutes).Sum() + secondsToMinutes;
+            int minutesToHours = totalMinutes / 60;
+            int minutes = totalMinutes % 60;
 
-            var hours = minutesToHours;
+            int hours = minutesToHours;
 
             Console.WriteLine($"Playlist length: {hours}h {minutes}m {seconds}s");
         }
 
         private static List<Song> GetSongs()
         {
-            var numberOfSongs = int.Parse(Console.ReadLine());
-            var songs = new List<Song>(numberOfSongs);
+            int numberOfSongs = int.Parse(Console.ReadLine());
+            List<Song> songs = new List<Song>(numberOfSongs);
 
             for (int i = 0; i < numberOfSongs; i++)
             {
@@ -45,10 +45,8 @@
 
                     string artist = inputLine[0];
                     string songName = inputLine[1];
-                    int songMinutes = int.Parse
-                        (inputLine[2].Substring(0, songTime));
-                    int songSeconds = int.Parse
-                        (inputLine[2].Substring(songTime + 1));
+                    int songMinutes = int.Parse(inputLine[2].Substring(0, songTime));
+                    int songSeconds = int.Parse(inputLine[2].Substring(songTime + 1));
 
                     songs.Add(new Song(artist, songName, songMinutes, songSeconds));
                     Console.WriteLine("Song added.");
