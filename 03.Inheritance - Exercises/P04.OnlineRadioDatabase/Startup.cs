9f1143b65791ba092d248ddbfde5049b5a798f53
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-03-11 10:58:47 +0200

    initial commit

--- 03.Inheritance - Exercises/P04.OnlineRadioDatabase/Startup.cs
+++ 03.Inheritance - Exercises/P04.OnlineRadioDatabase/Startup.cs
@@ -1,33 +1,64 @@
 ﻿namespace P04.OnlineRadioDatabase
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
     public class Startup
     {
         public static void Main()
         {
-            int numberOfSongs = int.Parse(Console.ReadLine());
+            var songs = GetSongs();
+            PrintPlaylistSummary(songs);
+        }
+
+        private static void PrintPlaylistSummary(List<Song> playlist)
+        {
+            Console.WriteLine($"Songs added: {playlist.Count}");
+
+            var totalSeconds = playlist.Select(s => s.SongSeconds).Sum();
+            var secondsToMinutes = totalSeconds / 60;
+            var seconds = totalSeconds % 60;
+
+            var totalMinutes = playlist.Select(s => s.SongMinutes).Sum() + secondsToMinutes;
+            var minutesToHours = totalMinutes / 60;
+            var minutes = totalMinutes % 60;
+
+            var hours = minutesToHours;
+
+            Console.WriteLine($"Playlist length: {hours}h {minutes}m {seconds}s");
+        }
+
+        private static List<Song> GetSongs()
+        {
+            var numberOfSongs = int.Parse(Console.ReadLine());
+            var songs = new List<Song>(numberOfSongs);
 
             for (int i = 0; i < numberOfSongs; i++)
             {
-                string[] inputLine = Console.ReadLine().Split(";");
-                string artist = inputLine[0];
-                string songName = inputLine[1];
-                string[] songTime = inputLine[2].Split(":");
-                int songMinutes = int.Parse(songTime[0]);
-                int songSeconds = int.Parse(songTime[1]);
+                string[] inputLine = Console.ReadLine().Split(';');
 
-                Song song = new Song(artist, songName, songMinutes, songSeconds);
-            }
+                try
+                {
+                    int songTime = inputLine[2].IndexOf(':');
 
-            try
-            {
-                Console.WriteLine(song);
-            }
-            catch (ArgumentException ex)
-            {
 
-                Console.WriteLine(ex.Message);
+                    string artist = inputLine[0];
+                    string songName = inputLine[1];
+                    int songMinutes = int.Parse
+                        (inputLine[2].Substring(0, songTime));
+                    int songSeconds = int.Parse
+                        (inputLine[2].Substring(songTime + 1));
+
+                    songs.Add(new Song(artist, songName, songMinutes, songSeconds));
+                    Console.WriteLine("Song added.");
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
+            return songs;
         }
     }
 }
