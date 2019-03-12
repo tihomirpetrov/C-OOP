dce461c01fb1ec1ef5b0597022b1ae4219c86864
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-03-12 19:52:52 +0200

    initial commit

---
+++ 04.Interfaces and Abstraction - Lab/P01.Shapes/Circle.cs
@@ -0,0 +1,37 @@
+﻿namespace Shapes
+{
+    using System;
+    public class Circle : IDrawable
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public void Draw()
+        {
+            double radiusIn = this.radius - 0.4;
+            double radiusOut = this.radius + 0.4;
+
+            for (double y = this.radius; y >= -this.radius; --y)
+            {
+                for (double x = -this.radius; x < radiusOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+
+                    if (value >= radiusIn * radiusIn && value <= radiusOut * radiusOut)
+                    {
+                        Console.WriteLine("*");
+                    }
+                    else
+                    {
+                        Console.WriteLine(" ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ 04.Interfaces and Abstraction - Lab/P01.Shapes/Rectangle.cs
@@ -0,0 +1,60 @@
+﻿namespace Shapes
+{
+    using System;
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int height)
+        {
+            this.Width = width;
+            this.height = height;
+        }
+
+        public int Width
+        {
+            get
+            {
+                return this.width;
+            }
+            set
+            {
+                this.width = value;
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return this.height;
+            }
+            set
+            {
+                this.height = value;
+            }
+        }
+
+        public void Draw()
+        {
+            DrawLine(this.width, '*', '*');
+            for (int i = 1; i < this.height - 1; ++i)
+            {
+                DrawLine(this.width, '*', ' ');
+            }
+            DrawLine(this.width, '*', '*');
+        }
+
+        private void DrawLine(int width, char end, char mid)
+        {
+            Console.WriteLine(end);
+
+            for (int i = 1; i < width - 1; ++i)
+            {
+                Console.Write(mid);
+            }
+            Console.WriteLine(end);
+        }
+    }
+}
