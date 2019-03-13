a6c992c477e74f1c08c5e8d8de3497c89a7be1fe
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-03-14 00:52:39 +0200

    initial commit

---
+++ 04.Interfaces and Abstraction - Exercises/P04.Telephony/Smartphone.cs
@@ -0,0 +1,61 @@
+﻿namespace Telephony
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    public class Smartphone : ICall, IBrowse
+    {
+        public Smartphone(string[] phoneNumbers, string[] sites)
+        {
+            this.PhoneNumbers = phoneNumbers;
+            this.Sites = sites;
+        }
+
+        public string[] PhoneNumbers { get; private set; }
+
+        public string[] Sites { get; private set; }
+
+        public string BrowseTheWeb()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in this.Sites)
+            {
+                if (item.Any(char.IsDigit))
+                {
+                    sb.AppendLine($"Invalid URL!");
+                }
+                else
+                {
+                    sb.AppendLine($"Browsing: {item}!");
+                }
+            }
+            return sb.ToString();
+        }
+
+        public string CallOtherNumbers()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in this.PhoneNumbers)
+            {
+                if (item.Any(char.IsLetter))
+                {
+                    sb.AppendLine($"Invalid number!");
+                }
+                else
+                {
+                    sb.AppendLine($"Calling... {item}");
+                }
+            }
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(CallOtherNumbers().ToString().TrimEnd());
+            sb.Append(BrowseTheWeb().ToString().TrimEnd());
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ 04.Interfaces and Abstraction - Exercises/P04.Telephony/Startup.cs
@@ -0,0 +1,18 @@
+﻿namespace Telephony
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            string[] phoneNumbers = Console.ReadLine().Split();
+            string[] urls = Console.ReadLine().Split();
+
+            Smartphone smartphone = new Smartphone(phoneNumbers, urls);
+            Console.WriteLine(smartphone);
+
+        }
+    }
+}
