552ccd796b4592b798f7869cac2f213f46fd8162
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-03-18 22:02:32 +0200

    initial commit

---
+++ 05.Polymorphism - Lab/P02.Animals/Animal.cs
@@ -0,0 +1,19 @@
+﻿namespace Animals
+{
+    public class Animal
+    {
+        protected Animal(string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favouriteFood;
+        }
+        public string Name { get; protected set; }
+        public string FavouriteFood { get; protected set; }
+
+
+        public virtual string ExplainSelf()
+        {
+            return $"I'm {this.Name} and my favourite food is {this.FavouriteFood}";
+        }
+    }
+}
