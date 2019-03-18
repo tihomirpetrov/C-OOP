59bd77c3852347a1831dc3a1a54f174ce6d1f388
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-03-18 23:09:48 +0200

    initial commit

---
+++ 05.Polymorphism - Lab/P03.Shapes/Shape.cs
@@ -0,0 +1,14 @@
+﻿namespace P03.Shapes
+{
+    public abstract class Shape
+    {
+        public abstract double CalculatePerimeter();
+
+        public abstract double CalculateArea();
+
+        public virtual string Draw()
+        {
+            return null;
+        }
+    }
+}
