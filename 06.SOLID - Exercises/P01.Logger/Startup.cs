e4261fe46f0ac277890a2a1e6599f2f2a57eb2f6
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-03-21 13:32:15 +0200

    initial commit

--- 06.SOLID - Exercises/P01.Logger/Startup.cs
+++ 06.SOLID - Exercises/P01.Logger/Startup.cs
@@ -1,13 +1,18 @@
 ﻿namespace P01.Logger
 {
-    using System;
+    using P01.Logger.Appenders;
+    using P01.Logger.Appenders.Contracts;
+    using P01.Logger.Layouts;
+    using P01.Logger.Layouts.Contracts;
+    using P01.Logger.Loggers.Contracts;
+    using P01.Logger.Loggers;
+    
     public class Startup
     {
         public static void Main()
         {
             ILayout simpleLayout = new SimpleLayout();
-            IAppender consoleAppender =
-            new ConsoleAppender(simpleLayout);
+            IAppender consoleAppender = new ConsoleAppender(simpleLayout);
             ILogger logger = new Logger(consoleAppender);
 
             logger.Error("3/26/2015 2:08:11 PM", "Error parsing JSON.");
