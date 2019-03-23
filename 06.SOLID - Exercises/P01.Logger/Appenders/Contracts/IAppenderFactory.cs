d5158671456a55f1a5a9267ce25705e447631796
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-03-24 01:50:33 +0200

    initial commit

---
+++ 06.SOLID - Exercises/P01.Logger/Appenders/Contracts/IAppenderFactory.cs
@@ -0,0 +1,8 @@
+﻿namespace P01.Logger.Appenders.Contracts
+{
+    using P01.Logger.Layouts.Contracts;
+    public interface IAppenderFactory
+    {
+        IAppender CreateAppender(string type, ILayout layout);
+    }
+}
