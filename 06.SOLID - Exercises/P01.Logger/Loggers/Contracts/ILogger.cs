db187359142f88df50241f2d303c7a6b58f80265
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-03-23 23:00:41 +0200

    initial commit

--- 06.SOLID - Exercises/P01.Logger/Loggers/Contracts/ILogger.cs
+++ 06.SOLID - Exercises/P01.Logger/Loggers/Contracts/ILogger.cs
@@ -3,7 +3,9 @@
     public interface ILogger
     {
         void Error(string dateTime, string errorMessage);
-
+        void Warning(string dateTime, string warningMessage);
+        void Critical(string dateTime, string criticalMessage);
+        void Fatal(string dateTime, string fatalMessage);        
         void Info(string dateTime, string infoMessage);
     }
 }
--- 06.SOLID - Exercises/P01.Logger/Startup.cs
+++ 06.SOLID - Exercises/P01.Logger/Startup.cs
@@ -12,11 +12,11 @@
     {
         public static void Main()
         {
-            var simpleLayout = new SimpleLayout();
-            var consoleAppender = new ConsoleAppender(simpleLayout);
+            ILayout simpleLayout = new SimpleLayout();
+            IAppender consoleAppender = new ConsoleAppender(simpleLayout);
             consoleAppender.ReportLevel = ReportLevel.Error;
 
-            var logger = new Logger(consoleAppender);
+            ILogger logger = new Logger(consoleAppender);
 
             logger.Info("3/31/2015 5:33:07 PM", "Everything seems fine");
             logger.Warning("3/31/2015 5:33:07 PM", "Warning: ping is too high - disconnect imminent");
