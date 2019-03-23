d8d60e9f06eb6bede3c7cd8acf21340c4acd137d
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-03-24 01:14:50 +0200

    initial commit

--- 06.SOLID - Exercises/P01.Logger/Core/CommandInterpreter.cs
+++ 06.SOLID - Exercises/P01.Logger/Core/CommandInterpreter.cs
@@ -1,11 +1,32 @@
 ﻿namespace P01.Logger.Core
 {
+    using P01.Logger.Appenders.Contracts;
     using P01.Logger.Core.Contracts;
+    using P01.Logger.Loggers.Enums;
+    using System;
+    using System.Collections.Generic;
+
     public class CommandInterpreter : ICommandInterpreter
     {
+        private ICollection<IAppender> appenders;
+
+        public CommandInterpreter()
+        {
+            this.appenders = new List<IAppender>();
+        }
+
         public void AddAppender(string[] args)
         {
-            throw new System.NotImplementedException();
+            string typeAppender = args[0];
+            string typeLayout = args[1];
+            ReportLevel reportLevel = ReportLevel.Info;
+
+            if (args.Length == 3)
+            {
+                reportLevel = Enum.Parse<ReportLevel>(args[2]);
+            }
+
+
         }
 
         public void AddReport(string[] args)
