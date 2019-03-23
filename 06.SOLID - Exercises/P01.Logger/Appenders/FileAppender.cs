fac1dd46f5c27b70d4ccf5a661af1f5d438a283e
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-03-23 20:17:35 +0200

    initial commit

---
+++ 06.SOLID - Exercises/P01.Logger/Appenders/FileAppender.cs
@@ -0,0 +1,11 @@
+﻿namespace P01.Logger.Appenders
+{
+    using P01.Logger.Appenders.Contracts;
+    public class FileAppender : IAppender
+    {
+        public void Append(string dateTime, string reportLevel, string message)
+        {
+            
+        }
+    }
+}
