b52e63e26fb5a942be7912600851e47c95388bdf
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-03-26 10:26:43 +0200

    initial commit

---
+++ 07.Reflection and Attributes - Exercises/P04.BarrackWars-TheCommandsStrikeBack/Core/CommandInterpreter.cs
@@ -0,0 +1,15 @@
+﻿namespace P04.BarrackWars_TheCommandsStrikeBack.Core
+{
+    using P04.BarrackWars_TheCommandsStrikeBack.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public IExecutable InterpretCommand(string[] data, string commandName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
