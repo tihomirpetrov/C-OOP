0273f9d0bbe4e340839393f3491d1437962c8a4e
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-03-30 20:35:02 +0200

    initial commit

--- 09.Unit testing - Lab/Skeleton.Tests/AxeTests.cs
+++ 09.Unit testing - Lab/Skeleton.Tests/AxeTests.cs
@@ -1,15 +1,17 @@
-﻿namespace Tests
+﻿namespace Skeleton.Tests
 {
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
     public class AxeTests
     {
-        [Test]
         public void AxeLooseDurabilityAfterAttack()
         {
             Axe axe = new Axe(10, 10);
             Dummy dummy = new Dummy(10, 10);
 
             axe.Attack(dummy);
-
             Assert.That(axe.DurabilityPoints, Is.EqualTo(9), "Axe Durability doesn't change after attack.");
         }
     }
