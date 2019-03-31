c23013ef23681beec21aca2299b89f9dfb171c97
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-03-31 17:56:33 +0300

    initial commit

--- 09.Unit testing - Lab/Skeleton.Tests/DummyTests.cs
+++ 09.Unit testing - Lab/Skeleton.Tests/DummyTests.cs
@@ -14,6 +14,12 @@
             Assert.That(dummy.Health, Is.EqualTo(5));
         }
 
+        [Test]
+        public void DeadDummyThrowExceptionIfAttacked()
+        {
+            Dummy dummy = new Dummy(0, 10);
 
+            Assert.That(() => dummy.TakeAttack(5), Throws.InvalidOperationException.With.Message.EqualTo("Dummy is dead."));
+        }
     }
 }
