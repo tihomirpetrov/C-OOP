dec9793b488f4e98388d7645f8c006334c6814da
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-04-02 10:23:47 +0300

    initial commit

---
+++ 09.Unit testing - Exercises/P01.Database/Database.cs
@@ -0,0 +1,36 @@
+﻿namespace P01.Database
+{
+    using System;
+
+    public class Database
+    {
+        private int[] database;
+        private int numbers;
+
+        public Database(int[] database)
+        {
+            if (database.Length > 16)
+            {
+                throw new InvalidOperationException();
+            }
+
+            this.database = new int[16];
+            this.numbers = database.Length;
+        }
+
+        public void Add()
+        {
+
+        }
+
+        public void Remove()
+        {
+
+        }
+
+        public int[] Fetch()
+        {
+            return null;
+        }
+    }
+}
