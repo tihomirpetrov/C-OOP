a3090a62a84c999b018e75a4d4e88433ec27d75c
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-04-02 10:06:30 +0300

    initial commit

--- 09.Unit testing - Lab/Skeleton/Tests/AxeTests.cs
+++ 09.Unit testing - Lab/Skeleton/Tests/AxeTests.cs
@@ -1,16 +1,13 @@
-﻿namespace Tests
+﻿public class AxeTests
 {
-    public class AxeTests
+    [Test]
+    public void AxeLooseDurabilityAfterAttack()
     {
-        [Test]
-        public void AxeLooseDurabilityAfterAttack()
-        {
-            Axe axe = new Axe(10, 10);
-            Dummy dummy = new Dummy(10, 10);
+        Axe axe = new Axe(10, 10);
+        Dummy dummy = new Dummy(10, 10);
 
-            axe.Attack(dummy);
+        axe.Attack(dummy);
 
-            Assert.That(axe.DurabilityPoints, Is.EqualTo(9), "Axe Durability doesn't change after attack.");
-        }
+        Assert.That(axe.DurabilityPoints, Is.EqualTo(9), "Axe Durability doesn't change after attack.");
     }
 }
