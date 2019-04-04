58926c8ccc415ad54ff3686fa75dfd1cce9c2b7d
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-04-04 06:42:33 +0300

    initial commit

---
+++ 09.Unit testing - Exercises/P02.ExtendedDatabas/Repository/Database.cs
@@ -0,0 +1,46 @@
+﻿namespace P02.ExtendedDatabas.Repository
+{
+    using P02.ExtendedDatabas.Interfaces;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Database
+    {
+        private HashSet<IPerson> people;
+
+        public Database()
+        {
+            this.people = new HashSet<IPerson>();
+        }
+
+        public Database(IEnumerable<IPerson> people) 
+            : this()
+        {
+            if (people != null)
+            {
+                foreach (var person in people)
+                {
+                    this.Add(person);
+                }
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.people.Count;
+            }
+        }
+        private void Add(IPerson person)
+        {
+            if (this.people.Any(x =>x.Id == person.Id || x.Username == person.Username))
+            {
+                throw new InvalidOperationException();
+            }
+
+            this.people.Add(person);
+        }
+    }
+}
