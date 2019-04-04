653c8da5b06f2574877404982f69fe9ed26e4a72
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-04-04 06:35:44 +0300

    initial commit

---
+++ 09.Unit testing - Exercises/P02.ExtendedDatabas/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace P02.ExtendedDatabas.Models
+{
+    using P02.ExtendedDatabas.Interfaces;
+    public class Person : IPerson
+    {
+        public Person(long id, string username)
+        {
+            this.Id = id;
+            this.Username = username;
+        }
+
+        public long Id { get; private set; }
+        public string Username { get; private set; }
+    }
+}
