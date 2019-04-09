bf868b5b571439e875e3b895f18ed49c38a652a8
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-04-09 13:01:43 +0300

    initial commit

---
+++ Exam preparation/AnimalCentre/Models/Animals/Animal.cs
@@ -0,0 +1,68 @@
+﻿namespace AnimalCentre.Models.Animals
+{
+    using AnimalCentre.Models.Contracts;
+    using System;
+
+    public abstract class Animal : IAnimal
+    {
+        private int energy;
+        private int happiness;
+        private const string defaultOwner = "Centre";
+
+        protected Animal(string name, int energy, int happiness, int procedureTime)
+        {
+            this.Name = name;
+            this.Energy = energy;
+            this.Happiness = happiness;
+            this.ProcedureTime = procedureTime;
+            this.Owner = defaultOwner;
+            this.IsAdopt = false;
+            this.IsChipped = false;
+            this.IsVaccinated = false;
+        }
+
+        public string Name { get; }
+        public int Happiness
+        {
+            get
+            {
+                return happiness;
+            }
+
+            private set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentException("Invalid happiness");
+                }
+                happiness = value;
+            }
+        }
+        //•	Happiness – int (can’t be less than 0 or more than 100. In these cases throw ArgumentException with message "Invalid happiness")
+
+        public int Energy
+        {
+            get
+            {
+                return energy;
+            }
+
+            private set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentException("Invalid energy");
+                }
+                energy = value;
+            }
+        }
+        //•	Energy – int (can’t be less than 0 or more than 100. In these cases throw ArgumentException with message "Invalid energy")
+
+
+        public int ProcedureTime { get; set; }
+        public string Owner { get; set; }
+        public bool IsAdopt { get; set; }
+        public bool IsChipped { get; set; }
+        public bool IsVaccinated { get; set; }
+    }
+}
---
+++ Exam preparation/AnimalCentre/Models/Contracts/IAnimal.cs
@@ -0,0 +1,30 @@
+﻿namespace AnimalCentre.Models.Contracts
+{
+    public interface IAnimal
+    {
+        string Name { get; } //private set ?
+        int Happiness { get; }
+
+        int Energy { get; }
+
+        int ProcedureTime { get; }
+
+        string Owner { get; }
+
+        bool IsAdopt { get; }
+
+        bool IsChipped { get; }
+
+        bool IsVaccinated { get; }
+
+//•	Name – string
+//•	Happiness – int (can’t be less than 0 or more than 100. In these cases throw ArgumentException with message "Invalid happiness")
+//•	Energy – int (can’t be less than 0 or more than 100. In these cases throw ArgumentException with message "Invalid energy")
+//•	ProcedureTime – int
+//•	Owner – string (by default: "Centre")
+//•	IsAdopt – bool (by default: false)
+//•	IsChipped – bool (by default: false)
+//•	IsVaccinated – bool (by default: false)
+
+    }
+}
