7c83d0d154dcb5761866815adff9c65edabf7e36
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-04-14 15:45:00 +0300

    initial commit

---
+++ Exam/MortalEngines/Entities/BaseMachines/Tank.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace MortalEngines.Entities.BaseMachines
+{
+    public class Tank : BaseMachine
+    {
+        private const double initialHealthPoints = 100;
+
+        public Tank(string name, double attackPoints, double defensePoints) 
+            : base(name, attackPoints, defensePoints, initialHealthPoints)
+        {
+            this.DefenseMode = true;
+        }
+
+        public bool DefenseMode { get; set; }
+
+        public void ToggleDefenseMode()
+        {
+            if (DefenseMode)
+            {
+                this.AttackPoints -= 40;
+                this.DefensePoints += 30;
+            }
+            else if (!DefenseMode)
+            {
+                this.AttackPoints += 40;
+                this.DefensePoints -= 30;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(base.ToString());
+            if (DefenseMode)
+            {
+                sb.AppendLine($" *Defense: ON");
+            }
+            else
+            {
+                sb.AppendLine($" *Defense: OFF");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
--- Exam/MortalEngines/IO/Contracts/IReader.cs
+++ Exam/MortalEngines/IO/Contracts/IReader.cs
@@ -2,6 +2,7 @@
 {
     using MortalEngines.Core.Contracts;
     using System.Collections.Generic;
+    using System.Windows.Input;
 
     public interface IReader
     {
